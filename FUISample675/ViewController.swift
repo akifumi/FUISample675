1e382ac0c2279c4221c8b32048b1b7b55f4bd104
Akifumi Fukaya <akifumi.fukaya@gmail.com>
2019-05-09 07:47:24 +0900

    Init commit

---
+++ FUISample675/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FUISample675
+//
+//  Created by akifumi.fukaya on 2019/05/09.
+//  Copyright © 2019 Akifumi Fukaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
