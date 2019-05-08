c74d3fce7340a11d9070ec6f1dbab54e5d92655f
Akifumi Fukaya <akifumi.fukaya@gmail.com>
2019-05-09 08:24:27 +0900

    Created RootViewController.swift & RootViewController.xib

---
+++ FUISample675/RootViewController.swift
@@ -0,0 +1,39 @@
+//
+//  RootViewController.swift
+//  FUISample675
+//
+//  Created by akifumi.fukaya on 2019/05/09.
+//  Copyright © 2019 Akifumi Fukaya. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: .main)
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
