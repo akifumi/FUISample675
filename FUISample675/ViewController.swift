533767a0f6076c17f02656d2545cd030f63fbb56
Akifumi Fukaya <akifumi.fukaya@gmail.com>
2019-05-09 08:19:10 +0900

    Implemented anonymous signin.

--- FUISample675/ViewController.swift
+++ FUISample675/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import FirebaseUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        let auth = Auth.auth()
+        if let user = auth.currentUser {
+            print("user uid : \(user.uid), isAnonymous ? \(user.isAnonymous)")
+            openRootScene()
+            return
+        }
+
+        auth.signInAnonymously() { [weak self] (authResult, error) in
+            guard let self = self else { return }
+            if let error = error {
+                print("Error: \(error)")
+                return
+            }
+            guard let user = authResult?.user else {
+                print("Error: user not found")
+                return
+            }
+            print("user uid : \(user.uid), isAnonymous ? \(user.isAnonymous)")
+
+            self.openRootScene()
+        }
     }
 
-
+    private func openRootScene() {
+    }
 }
-
